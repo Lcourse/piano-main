db2d86c33c45f05532b0aea0f503aadc988f0996
Lcourse <101373417+Lcourse@users.noreply.github.com>
2023-08-18 00:08:20 -0300

    const handleClick

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const keys = document.querySelectorAll(".key");
+
+const handleClick = () => {
+
+}
+
+keys.forEach((key) => {
+
+    key.addEventListener('click', () => console.log
+    ('click'))
+});
