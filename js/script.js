a8ec42a428de6717bd57db7d0f7bd6e164f03646
Lcourse <101373417+Lcourse@users.noreply.github.com>
2023-08-19 01:44:36 -0300

    remove-and-add-keys

--- js/script.js
+++ js/script.js
@@ -1,47 +1,49 @@
 const keys = document.querySelectorAll(".key");
-const checkbox = document.querySelector('.checkbox__keys')
-const switcher = document.querySelector('.switcher')
+const checkbox = document.querySelector(".checkbox__keys");
+const switcher = document.querySelector(".switcher");
+const keysSection = document.querySelector('.piano__keys')
 
 /* Função de tocar a nota */
 const playNote = (note) => {
-    const audio = new Audio(`../notes/${note}.wav`)
-    audio.play()
-
-}
+  const audio = new Audio(`../notes/${note}.wav`);
+  audio.play();
+};
 /* Função que apertar a tecla e estilização #ddd */
 const handleMouseDown = (key) => {
-    playNote(key.getAttribute('data-note'))
+  playNote(key.getAttribute("data-note"));
 
-    if (key.className.includes('black')) {
-        key.classList.add('black--pressed')
-        return;
-    }
+  if (key.className.includes("black")) {
+    key.classList.add("black--pressed");
+    return;
+  }
 
-    key.style.background = '#ddd'
-}
+  key.style.background = "#ddd";
+};
 
 /* Função que apertar a tecla e estilização #ddd */
 const handleMouseUp = (key) => {
+  if (key.className.includes("black")) {
+    key.classList.remove("black--pressed");
+    return;
+  }
 
-    if (key.className.includes('black')) {
-        key.classList.remove('black--pressed')
-        return;
-    }
-    
-    key.style.background = 'white';
-}
+  key.style.background = "white";
+};
 
 keys.forEach((key) => {
+  key.addEventListener("mousedown", () => handleMouseDown(key));
 
-
-    key.addEventListener('mousedown', () => handleMouseDown(key))
-
-    key.addEventListener('mouseup', () => handleMouseUp(key))
+  key.addEventListener("mouseup", () => handleMouseUp(key));
 });
 
-checkbox.addEventListener('change', ({ target }) => {
-    if (target.checked) {
-        switcher.classList.add('switcher--active')
-    }
+/* Ativar e desativar a visualização das teclas */
+checkbox.addEventListener("change", ({ target }) => {
+  if (target.checked) {
+    switcher.classList.add("switcher--active");
+    keysSection.classList.remove('disabled-keys')
+    return;
+  }
 
-})
+  switcher.classList.remove('switcher--active');
+  keysSection.classList.add('disabled-keys')
+});
